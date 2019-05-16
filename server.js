1ba0f69c88d4e737f9ee60a75a7128ac754df274
Andreas Røste <andreas.roste@rugge.org>
2019-05-16 20:01:05 +0200

    force https

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const port = process.env.PORT || 3000
 
 const express = require('express')
 const path = require('path')
+const requireHttps = require('require-https')
 const app = express()
 
 const backend = require('./backend/index')
+
+// Force HTTPS
+app.use(requireHttps());
+app.enable('trust proxy');
 
 
 app.use('/api', backend)
