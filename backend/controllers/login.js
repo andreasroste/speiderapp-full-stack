d8b7b43b660aab2f5ed601afc7f7cd3589f609af
Andreas Røste <andreas.roste@rugge.org>
2020-07-08 21:58:56 +0200

    rollbar test

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -21,7 +21,7 @@ module.exports = async (req, res, next) => {
             app_name: 'Speiderappen (app.speiding.no)',
             device_name: req.get('user-agent')
         })
-        rollbar.log(auth_request);
+        rollbar.log(auth_request, req);
         req.session.user = {
             scoutnet_token: auth_request.data.token,
             app_access: [],
